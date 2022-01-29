1990daffdb86f283d6ec3644bc0b7c584e28ccff
vitalir2 <vitalyi100@mail.ru>
2022-01-29 14:23:10 +0300

    Experiment with BookStoreLogo

--- webfrontend/src/main/kotlin/components/BookStoreLogo.kt
+++ webfrontend/src/main/kotlin/components/BookStoreLogo.kt
@@ -1,13 +1,34 @@
 package components
 
+import csstype.ColorProperty
+import kotlinext.js.jso
 import mui.icons.material.LocalLibrary
+import mui.material.Stack
+import mui.material.StackDirection
+import mui.material.SvgIconColor
+import mui.material.Typography
+import mui.system.ResponsiveStyleValue
 import react.FC
 import react.Props
-import react.dom.html.ReactHTML.h1
 
 val BookStoreLogo = FC<Props> {
-    h1 {
-        +"BookStore"
+    Stack {
+        sx = jso {
+            backgroundColor = "#B71C1C".unsafeCast<ColorProperty>()
+        }
+        direction = ResponsiveStyleValue(StackDirection.row)
+        spacing = ResponsiveStyleValue(2)
+
+        Typography {
+            sx = jso {
+                color = "white".unsafeCast<ColorProperty>()
+            }
+            variant = "h4"
+
+            +"BookStore"
+        }
+        LocalLibrary {
+            color = SvgIconColor.primary
+        }
     }
-    LocalLibrary()
 }
