ce1757f6c04e06b80c0c22bdefeb30925b41084b
vitalir2 <vitalyi100@mail.ru>
2022-01-29 11:15:26 +0300

    Improve main function, remove legacy code from BookStoreApp component

--- webfrontend/src/main/kotlin/main.kt
+++ webfrontend/src/main/kotlin/main.kt
@@ -1,11 +1,17 @@
+import components.BookStoreLogo
 import kotlinx.browser.document
-import kotlinx.browser.window
+import react.FC
+import react.Props
+import react.create
 import react.dom.render
 
 fun main() {
-    window.onload = {
-        render(document.getElementById("root")!!) {
-            child(BookStoreApp)
-        }
-    }
+    render(
+        element = BookStoreApp.create(),
+        container = document.createElement("div").also { document.body!!.appendChild(it) }
+    )
+}
+
+private val BookStoreApp = FC<Props> {
+    BookStoreLogo()
 }
