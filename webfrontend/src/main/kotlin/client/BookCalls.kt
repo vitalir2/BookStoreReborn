f9df9e40cf77e561a642d7b22b0c8f28d9625707
vitalir2 <vitalyi100@mail.ru>
2022-02-06 20:38:57 +0300

    Make deserialization working in webfrontend, change list to array

--- webfrontend/src/main/kotlin/client/BookCalls.kt
+++ webfrontend/src/main/kotlin/client/BookCalls.kt
@@ -3,21 +3,26 @@ package client
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import model.Book
 
 const val DEFAULT_HOST = "http://0.0.0.0:8081"
 
 fun interface GetBooksUseCase {
-    suspend operator fun invoke(): List<Book>
+    suspend operator fun invoke(): Array<Book>
 }
 
 class GetBooksUseCaseImpl : GetBooksUseCase {
-    override suspend fun invoke(): List<Book> {
-        val booksResponse: HttpResponse = HttpClient.client.get("$DEFAULT_HOST/book")
+    override suspend fun invoke(): Array<Book> {
+        val booksResponse: HttpResponse = HttpClient.client.get("$DEFAULT_HOST/book") {
+            headers {
+                append(HttpHeaders.ContentType, "application/json")
+            }
+        }
         if (booksResponse.status.value !in 400..600) {
             return booksResponse.body()
         }
-        return emptyList()
+        return emptyArray()
     }
 }
 
--- webfrontend/src/main/kotlin/client/HttpClient.kt
+++ webfrontend/src/main/kotlin/client/HttpClient.kt
@@ -1,15 +1,19 @@
 package client
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 
 object HttpClient {
-    val scope: CoroutineScope = CoroutineScope(Job() + Dispatchers.Default)
+    val scope: CoroutineScope = CoroutineScope(Job())
 
     val client: HttpClient = HttpClient() {
         install(Logging)
+        install(ContentNegotiation) {
+            json()
+        }
     }
 }
