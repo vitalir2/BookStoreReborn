e3e7459f199843fd34be92e76cab60a5cf0894fe
vitalir2 <vitalyi100@mail.ru>
2022-02-17 17:39:42 +0300

    Add pagination book use case for webfrontend

--- webfrontend/src/main/kotlin/client/BookCalls.kt
+++ webfrontend/src/main/kotlin/client/BookCalls.kt
@@ -6,8 +6,6 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import model.Book
 
-const val DEFAULT_HOST = "http://0.0.0.0:8081"
-
 fun interface GetBooksUseCase {
     suspend operator fun invoke(): Array<Book>
 }
@@ -19,12 +17,33 @@ class GetBooksUseCaseImpl : GetBooksUseCase {
                 append(HttpHeaders.ContentType, "application/json")
             }
         }
-        if (booksResponse.status.value !in 400..600) {
+        if (booksResponse.status.value !in MIN_ERROR_CODE..MAX_ERROR_CODE) {
             return booksResponse.body()
         }
         return emptyArray()
     }
 }
 
+fun interface GetPaginatedBooksUseCase {
+    suspend operator fun invoke(offset: Long, limit: Int): Array<Book>
+}
+
+class GetPaginatedBooksUseCaseImpl : GetPaginatedBooksUseCase {
+    override suspend fun invoke(offset: Long, limit: Int): Array<Book> {
+        val booksResponse: HttpResponse = HttpClient.client.get(getPaginatedUrl(offset, limit)) {
+            headers {
+                append(HttpHeaders.ContentType, "application/json")
+            }
+        }
+        if (booksResponse.status.value !in MIN_ERROR_CODE..MAX_ERROR_CODE) {
+            return booksResponse.body()
+        }
+        return emptyArray()
+    }
+}
+
+private fun getPaginatedUrl(offset: Long, limit: Int): String = "$DEFAULT_HOST/book?offset=$offset&limit=$limit"
+
 // TODO: Change to DI like KodeIn
 val getBooksUseCase: GetBooksUseCase = GetBooksUseCaseImpl()
+val getPaginatedBooksUseCase: GetPaginatedBooksUseCase = GetPaginatedBooksUseCaseImpl()
