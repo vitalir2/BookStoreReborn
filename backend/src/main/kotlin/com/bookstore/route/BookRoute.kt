1e545f77a9b55c0d6cf9a5951bf70afc9583a794
vitalir2 <vitalyi100@mail.ru>
2022-05-10 16:52:08 +0300

    Refactor BookRoute.kt

--- backend/src/main/kotlin/com/bookstore/route/BookRoute.kt
+++ backend/src/main/kotlin/com/bookstore/route/BookRoute.kt
@@ -8,30 +8,39 @@ import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
 fun Route.bookRoute() {
-    val bookRepository by inject<BookRepository>()
     route("/book") {
-        get {
-            val queryParameters = call.request.queryParameters
-            if (queryParameters.isEmpty()) {
-                val books = bookRepository.getBooks()
-                call.respond(HttpStatusCode.Accepted, books)
-            } else {
-                val rawOffset = queryParameters["offset"] ?: "0"
-                val offset = rawOffset.toLongOrNull() ?: return@get call.respond(HttpStatusCode.BadRequest)
-                val rawLimit = queryParameters["limit"] ?: return@get call.respond(HttpStatusCode.BadRequest)
-                val limit = rawLimit.toIntOrNull() ?: return@get call.respond(HttpStatusCode.BadRequest)
-                val paginatedBooks = bookRepository.getBooksPaginated(offset, limit)
-                call.respond(HttpStatusCode.Accepted, paginatedBooks)
-            }
-        }
-        get("/{title}") {
-            val title = call.parameters["title"]
-                ?: return@get call.respond(HttpStatusCode.BadRequest)
+        getBooks()
+        getBookByTitle()
+    }
+}
 
-            val book = bookRepository.getBookByTitle(title)
-                ?: return@get call.respond(HttpStatusCode.NotFound)
-
-            call.respond(HttpStatusCode.Accepted, book)
+private fun Route.getBooks() {
+    val bookRepository by inject<BookRepository>()
+    get {
+        val queryParameters = call.request.queryParameters
+        if (queryParameters.isEmpty()) {
+            val books = bookRepository.getBooks()
+            call.respond(HttpStatusCode.Accepted, books)
+        } else {
+            val rawOffset = queryParameters["offset"] ?: "0"
+            val offset = rawOffset.toLongOrNull() ?: return@get call.respond(HttpStatusCode.BadRequest)
+            val rawLimit = queryParameters["limit"] ?: return@get call.respond(HttpStatusCode.BadRequest)
+            val limit = rawLimit.toIntOrNull() ?: return@get call.respond(HttpStatusCode.BadRequest)
+            val paginatedBooks = bookRepository.getBooksPaginated(offset, limit)
+            call.respond(HttpStatusCode.Accepted, paginatedBooks)
         }
+    }
+}
+
+private fun Route.getBookByTitle() {
+    val bookRepository by inject<BookRepository>()
+    get("/{title}") {
+        val title = call.parameters["title"]
+            ?: return@get call.respond(HttpStatusCode.BadRequest)
+
+        val book = bookRepository.getBookByTitle(title)
+            ?: return@get call.respond(HttpStatusCode.BadRequest)
+
+        call.respond(HttpStatusCode.Accepted, book)
     }
 }
